31feaef3e7e6dff07df9f5a2613c6479fcd2f39a
Shinseph <animefanatic101@gmail.com>
2025-12-06 21:18:40 -0700

    Added Character counter

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import get_num_words
+from stats import get_num_words, character_count
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     num_words = get_num_words(book_path)
     print(f"Found {num_words} total words.")
+    characters = character_count(book_path)
+    print(characters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,19 @@
-def get_book_text(book_path):
-    with open(book_path) as file:
-        file_content = file.read()
-    return file_content
-
-def get_num_words(book_path):
-    text = get_book_text(book_path)
+def get_num_words(book_path: str) -> int:
+    with open(book_path) as f:
+        text = f.read()
     words = text.split()
     return len(words)
+
+
+def character_count(book_path: str) -> dict[str, int]:
+    with open(book_path) as f:
+        text = f.read()
+
+    count_dict = {}
+    for char in text.lower():
+        if char in count_dict:
+            count_dict[char] += 1
+        else:
+            count_dict[char] = 1
+
+    return dict(sorted(count_dict.items()))
